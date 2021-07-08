bf78ebf9ac11db1eb921188747f9428296c833ac
Tomas Bravo <tomas.b.c@outlook.com>
2021-07-08 18:59:54 -0400

    update mapAVL

--- MapAVL.cpp
+++ MapAVL.cpp
@@ -1,28 +1,124 @@
 #include "MapAVL.h"
+#include <stack>
 
-MapAVL ::MapAVL(int)
-{
-    mysize = 0;
-}
-
-MapAVL ::~MapAVL()
-{
+MapAVL ::MapAVL(int) {
+  mysize = 0;
+  root = nullptr;
 }
 
-void MapAVL ::insert(pair<string, int>)
-{
+MapAVL ::~MapAVL() {}
+
+nodo *MapAVL::new_node(pair<string, int> item) {
+  nodo *actual = new nodo();
+  actual->value = item.second;
+  actual->key = item.first;
+  actual->hijoizq = nullptr;
+  actual->hijoder = nullptr;
+  actual->height = 1;
+
+  return actual;
 }
 
-void MapAVL ::erase(string)
-{
+int height(nodo *actual) { return actual == nullptr ? 0 : actual->height; }
+
+nodo *MapAVL::dfs(nodo *actual, string key) {
+  if (!actual)
+    return actual;
+  dfs(actual->hijoizq, key);
+
+  dfs(actual->hijoder, key);
+
+  actual->height = 1 + max(height(actual->hijoizq), height(actual->hijoder));
+  int dif = height(actual->hijoizq) - height(actual->hijoder);
+
+  if (abs(dif) <= 1)
+    return actual;
+  // caso izquerda izquerda
+  if (dif > 1 && key < actual->hijoizq->key)
+    return rotacion_derecha(actual);
+  // caso derecha derecha
+  if (dif < -1 && key > actual->hijoder->key)
+    return rotacion_izquerda(actual);
+  // caso izquierda derecha
+  if (dif > 1 && key > actual->hijoizq->key) {
+    actual->hijoizq = rotacion_izquerda(actual->hijoizq);
+    return rotacion_derecha(actual);
+  }
+  // caso derecha izquerda
+  // if (dif < -1 && key < root->hijoder->key) {
+  actual->hijoder = rotacion_derecha(actual->hijoder);
+  return rotacion_izquerda(actual);
+  //}
 }
 
-int MapAVL ::at(string)
-{
+int max(int x, int y) { return x > y ? x : y; }
+
+void MapAVL ::balance(string key) {
+  if (root->key > key)
+    root->hijoizq = dfs(root->hijoizq, key);
+  else
+    root->hijoder = dfs(root->hijoder, key);
+  root->height = 1 + max(height(root->hijoizq), height(root->hijoder));
+  int dif = height(root->hijoizq) - height(root->hijoder);
+  if (abs(dif) <= 1)
+    return;
+  // caso izquierda izquierda
+  if (dif > 1 && key < root->hijoizq->key) {
+    root = rotacion_derecha(root);
+    return;
+  }
+  // caso derecha derecha
+  if (dif < -1 && key > root->hijoder->key) {
+    root = rotacion_izquerda(root);
+    return;
+  }
+  // izquerda derecha
+  if (dif > 1 && key > root->hijoizq->key) {
+    root->hijoizq = rotacion_izquerda(root->hijoizq);
+    root = rotacion_derecha(root);
+    return;
+  }
+  // derecha izquierda
+  if (dif < -1 && key < root->hijoder->key) {
+    root->hijoder = rotacion_derecha(root->hijoder);
+    root = rotacion_izquerda(root);
+  }
 }
-int MapAVL ::size()
-{
+
+void MapAVL ::insert(pair<string, int> item) {
+  nodo *actual = root;
+  stack<nodo *> pila;
+
+  while (actual != nullptr) {
+    if (actual->key == item.first)
+      return;
+    pila.push(actual);
+    if (actual->key > item.first)
+      actual = actual->hijoizq;
+    else
+      actual = actual->hijoder;
+  }
+  actual = new_node(item);
+  while (!pila.empty()) {
+    actual = pila.top();
+    pila.pop();
+    int balance = abs()
+  }
 }
-bool MapAVL ::empty()
-{
+
+void MapAVL ::erase(string) {}
+
+int MapAVL ::at(string key) {
+  nodo *actual = root;
+  while (actual != nullptr) {
+    if (actual->key == key)
+      return actual->value;
+    if (actual->key > key)
+      actual = actual->hijoizq;
+    else
+      actual = actual->hijoder;
+  }
+  return -1;
 }
+int MapAVL ::size() { return mysize; }
+bool MapAVL ::empty() { return mysize == 0; }
--- MapAVL.h
+++ MapAVL.h
@@ -1,9 +1,23 @@
 #include "ADTMap.h"
 
+struct nodo{
+    int value;
+    int height;
+    string key;
+    nodo *hijoizq;
+    nodo *hijoder;
+};
+
 class MapAVL : public ADTMap
 {
 private:
     int mysize;
+    nodo *root;
+    nodo *new_node(pair<string,int>); 
+    void balance(string);   
+    nodo* rotacion_derecha(nodo*);
+    nodo* rotacion_izquerda(nodo*);
+    nodo* dfs(nodo*, string);
 
 public:
     MapAVL(int);
