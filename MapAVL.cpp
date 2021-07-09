136ee28986115f686744f84a8638ff93d704b594
Tomas Bravo <tomas.b.c@outlook.com>
2021-07-08 20:46:10 -0400

    AVL terminado, espera de correcion de errores

--- MapAVL.cpp
+++ MapAVL.cpp
@@ -1,7 +1,7 @@
 #include "MapAVL.h"
 #include <stack>
 
-MapAVL ::MapAVL(int) {
+MapAVL ::MapAVL() {
   mysize = 0;
   root = nullptr;
 }
@@ -19,76 +19,106 @@ nodo *MapAVL::new_node(pair<string, int> item) {
   return actual;
 }
 
-int height(nodo *actual) { return actual == nullptr ? 0 : actual->height; }
-
-nodo *MapAVL::dfs(nodo *actual, string key) {
-  if (!actual)
-    return actual;
-  dfs(actual->hijoizq, key);
-
-  dfs(actual->hijoder, key);
-
-  actual->height = 1 + max(height(actual->hijoizq), height(actual->hijoder));
-  int dif = height(actual->hijoizq) - height(actual->hijoder);
-
-  if (abs(dif) <= 1)
-    return actual;
-  // caso izquerda izquerda
-  if (dif > 1 && key < actual->hijoizq->key)
-    return rotacion_derecha(actual);
-  // caso derecha derecha
-  if (dif < -1 && key > actual->hijoder->key)
-    return rotacion_izquerda(actual);
-  // caso izquierda derecha
-  if (dif > 1 && key > actual->hijoizq->key) {
-    actual->hijoizq = rotacion_izquerda(actual->hijoizq);
-    return rotacion_derecha(actual);
-  }
-  // caso derecha izquerda
-  // if (dif < -1 && key < root->hijoder->key) {
-  actual->hijoder = rotacion_derecha(actual->hijoder);
-  return rotacion_izquerda(actual);
-  //}
+int MapAVL::height(nodo *actual) {
+  return actual == nullptr ? 0 : actual->height;
 }
 
-int max(int x, int y) { return x > y ? x : y; }
+int MapAVL::max(int x, int y) { return x > y ? x : y; }
 
-void MapAVL ::balance(string key) {
-  if (root->key > key)
-    root->hijoizq = dfs(root->hijoizq, key);
-  else
-    root->hijoder = dfs(root->hijoder, key);
-  root->height = 1 + max(height(root->hijoizq), height(root->hijoder));
-  int dif = height(root->hijoizq) - height(root->hijoder);
-  if (abs(dif) <= 1)
-    return;
-  // caso izquierda izquierda
-  if (dif > 1 && key < root->hijoizq->key) {
-    root = rotacion_derecha(root);
-    return;
+void MapAVL::zigzigIzquierda(nodo *z, nodo *padre) {
+  nodo *y = z->hijoizq;
+  // rotacion hacia la derecha
+  z->hijoizq = y->hijoder;
+  y->hijoder = z;
+
+  // cambios de altura
+  z->height = 1 + max(height(z->hijoizq), height(z->hijoder));
+  y->height = 1 + max(height(y->hijoizq), height(y->hijoder));
+
+  if (padre) {
+    if (padre->hijoder == z)
+      padre->hijoder = y;
+    else
+      padre->hijoizq = y;
   }
-  // caso derecha derecha
-  if (dif < -1 && key > root->hijoder->key) {
-    root = rotacion_izquerda(root);
-    return;
+  if (z == root)
+    root = y;
+}
+void MapAVL::zigzigDerecha(nodo *z, nodo *padre) {
+  nodo *y = z->hijoder;
+  // rotacion hacia la derecha
+  z->hijoder = y->hijoizq;
+  y->hijoizq = z;
+
+  // cambios de altura
+  z->height = 1 + max(height(z->hijoizq), height(z->hijoder));
+  y->height = 1 + max(height(y->hijoizq), height(y->hijoder));
+
+  if (padre) {
+    if (padre->hijoder == z)
+      padre->hijoder = y;
+    else
+      padre->hijoizq = y;
   }
-  // izquerda derecha
-  if (dif > 1 && key > root->hijoizq->key) {
-    root->hijoizq = rotacion_izquerda(root->hijoizq);
-    root = rotacion_derecha(root);
-    return;
+  if (z == root)
+    root = y;
+}
+
+void MapAVL::zigzagDerecha(nodo *z, nodo *padre) {
+  nodo *y = z->hijoder;
+  nodo *x = y->hijoizq;
+
+  y->hijoizq = x->hijoder;
+  x->hijoder = y;
+  z->hijoder = x->hijoizq;
+  x->hijoizq = z;
+
+  z->height = 1 + max(height(z->hijoizq), height(z->hijoder));
+  y->height = 1 + max(height(y->hijoizq), height(y->hijoder));
+  x->height = 1 + max(height(x->hijoizq), height(x->hijoder));
+
+  if (padre) {
+    if (padre->hijoder == z)
+      padre->hijoder = x;
+    else
+      padre->hijoizq = x;
   }
-  // derecha izquierda
-  if (dif < -1 && key < root->hijoder->key) {
-    root->hijoder = rotacion_derecha(root->hijoder);
-    root = rotacion_izquerda(root);
+
+  if (z == root)
+    root = x;
+}
+
+void MapAVL::zigzagIzquierda(nodo *z, nodo *padre) {
+  nodo *y = z->hijoizq;
+  nodo *x = y->hijoder;
+
+  y->hijoder = x->hijoizq;
+  x->hijoizq = y;
+  z->hijoizq = x->hijoder;
+  x->hijoder = z;
+
+  z->height = 1 + max(height(z->hijoizq), height(z->hijoder));
+  y->height = 1 + max(height(y->hijoizq), height(y->hijoder));
+  x->height = 1 + max(height(x->hijoizq), height(x->hijoder));
+
+  if (padre) {
+    if (padre->hijoder == z)
+      padre->hijoder = x;
+    else
+      padre->hijoizq = x;
   }
+
+  if (z == root)
+    root = x;
 }
 
 void MapAVL ::insert(pair<string, int> item) {
+  if (root == nullptr) {
+    root = new_node(item);
+    return;
+  }
   nodo *actual = root;
   stack<nodo *> pila;
-
   while (actual != nullptr) {
     if (actual->key == item.first)
       return;
@@ -99,14 +129,106 @@ void MapAVL ::insert(pair<string, int> item) {
       actual = actual->hijoder;
   }
   actual = new_node(item);
+  mysize++;
   while (!pila.empty()) {
     actual = pila.top();
     pila.pop();
-    int balance = abs()
+    actual->height = 1 + max(height(actual->hijoizq), height(actual->hijoder));
+    int balance = abs(height(actual->hijoder) - height(actual->hijoizq));
+    if (balance > 1) {
+      // derecha
+      if (actual->key < item.first) { // derecha
+        // derecha
+        nodo *hijo = actual->hijoder;
+        if (hijo->key < item.first) { // derecha
+          // zig zig izquerda
+          zigzigDerecha(actual, pila.top());
+        }
+        // izquierda
+        else if (hijo->key > item.first) { // izquierda
+          // zig zag derecha
+          zigzagDerecha(actual, pila.top());
+        }
+      }
+      // izquerda
+      else if (actual->key > item.first) { // izquierda
+        nodo *hijo = actual->hijoizq;
+        // izquierda
+        if (hijo->key > item.first) { // izquierda
+          // zig zig derecha
+          if (actual == root) {
+            zigzigIzquierda(actual, nullptr);
+          }
+          zigzigIzquierda(actual, pila.top());
+        }
+        // derecha
+        else if (hijo->key < item.first) { // derecha
+          // zig zag izquierda derecha
+          zigzagIzquierda(actual, pila.top());
+        }
+      }
+    }
   }
 }
 
-void MapAVL ::erase(string) {}
+void MapAVL ::erase(string key) {
+  nodo *actual = root;
+  stack<nodo *> pila;
+
+  while (actual != nullptr) {
+    if (actual->key == key) {
+      delete actual;
+      mysize--;
+      break;
+    }
+    pila.push(actual);
+    if (actual->key > key)
+      actual = actual->hijoizq;
+    else
+      actual = actual->hijoder;
+  }
+
+  if (actual == nullptr)
+    return;
+
+  while (!pila.empty()) {
+    actual = pila.top();
+    pila.pop();
+    actual->height = 1 + max(height(actual->hijoizq), height(actual->hijoder));
+    int balance = abs(height(actual->hijoder) - height(actual->hijoizq));
+    if (balance > 1) {
+      // derecha
+      if (height(actual->hijoder) > height(actual->hijoizq)) { // derecha
+        // derecha
+        nodo *hijo = actual->hijoder;
+        if (height(hijo->hijoder) > height(hijo->hijoizq)) { // derecha
+          // zig zig izquerda
+          zigzigDerecha(actual, pila.top());
+        }
+        // izquierda
+        else if (height(hijo->hijoder) < height(hijo->hijoizq)) { // izquierda
+          // zig zag derecha
+          zigzagDerecha(actual, pila.top());
+        }
+      }
+      // izquerda
+      else if (height(actual->hijoder) < height(actual->hijoizq)) { // izquierda
+        nodo *hijo = actual->hijoizq;
+        // izquierda
+        if (height(hijo->hijoder) < height(hijo->hijoizq)) { // izquierda
+          // zig zig derecha
+          zigzigIzquierda(actual, pila.top());
+        }
+        // derecha
+        else if (height(hijo->hijoder) > height(hijo->hijoizq)) { // derecha
+          // zig zag izquierda derecha
+          zigzagIzquierda(actual, pila.top());
+        }
+      }
+      root = actual;
+    }
+  }
+}
 
 int MapAVL ::at(string key) {
   nodo *actual = root;
--- MapAVL.h
+++ MapAVL.h
@@ -1,30 +1,32 @@
 #include "ADTMap.h"
 
-struct nodo{
-    int value;
-    int height;
-    string key;
-    nodo *hijoizq;
-    nodo *hijoder;
+struct nodo {
+  int value;
+  int height;
+  string key;
+  nodo *hijoizq;
+  nodo *hijoder;
 };
 
-class MapAVL : public ADTMap
-{
+class MapAVL : public ADTMap {
 private:
-    int mysize;
-    nodo *root;
-    nodo *new_node(pair<string,int>); 
-    void balance(string);   
-    nodo* rotacion_derecha(nodo*);
-    nodo* rotacion_izquerda(nodo*);
-    nodo* dfs(nodo*, string);
+  int mysize;
+  nodo *root;
+  nodo *new_node(pair<string, int>);
+  void balance(string);
+  void zigzagIzquierda(nodo *, nodo *);
+  void zigzagDerecha(nodo *, nodo *);
+  void zigzigIzquierda(nodo *, nodo *);
+  void zigzigDerecha(nodo *, nodo *);
+  int height(nodo *);
+  int max(int, int);
 
 public:
-    MapAVL(int);
-    ~MapAVL();
-    void insert(pair<string, int>);
-    void erase(string);
-    int at(string);
-    int size();
-    bool empty();
+  MapAVL();
+  ~MapAVL();
+  void insert(pair<string, int>);
+  void erase(string);
+  int at(string);
+  int size();
+  bool empty();
 };
